a1a03b471157b6539810954b2a61e8890cb7fee5
liting <luz.liting@gmail.com>
2023-01-19 13:17:33 +0800

    chore: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.scss'
+import { Outlet, Route, Routes } from 'react-router-dom'
+import BiosingerHeader from './components/Header/Header'
 import Home from './pages/Home'
 
 function App() {
   return (
     <div className="biosinger w-full min-w-256 h-screen">
-      <Home></Home>
+      <BiosingerHeader></BiosingerHeader>
+      <Routes>
+        <Route index path='/' element={<Home />}></Route>
+      </Routes>
+      <main>
+        <Outlet></Outlet>
+      </main>
     </div>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,7 @@
-import BiosingerHeader from '../components/Header/Header'
-
 function Home() {
-  return <div className='h-full'>
-    <BiosingerHeader />
-    <main className='h-full mt-16 flex justify-center items-center'>
+  return <main className='h-full mt-16 flex justify-center items-center'>
       <span className='text-6xl'>Bio Singer</span>
     </main>
-  </div>
 }
 
 export default Home
