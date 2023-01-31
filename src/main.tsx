e4ca6d1904110e0c1ce217f2ac774641e42f4843
liting <luz.liting@gmail.com>
2023-02-01 01:42:39 +0800

    style(unocss): unimport virtual:unocss-devtools

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import 'antd/dist/reset.css'
 import 'uno.css'
-import 'virtual:unocss-devtools'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
