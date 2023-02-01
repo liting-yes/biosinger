d6921d0b331d776cf5dd2e484f49945f88a39fda
liting <luz.liting@gmail.com>
2023-02-02 01:18:27 +0800

    build(vite): add vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import UnoCSS from '@unocss/vite'
 import legacy from '@vitejs/plugin-legacy'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
@@ -10,6 +11,7 @@ export default defineConfig({
     legacy({
       targets: ['defaults', 'not IE 11'],
     }),
+    VitePWA({ registerType: 'autoUpdate' }),
   ],
   css: {
     devSourcemap: true,
