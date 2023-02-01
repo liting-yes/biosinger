0097d25c0c86f822dd13d19a301159494c1665ff
liting <luz.liting@gmail.com>
2023-02-02 01:11:09 +0800

    build: add @vitejs/plugin-legacy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import UnoCSS from '@unocss/vite'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
   plugins: [
     UnoCSS(),
     react(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
   ],
   css: {
     devSourcemap: true,
