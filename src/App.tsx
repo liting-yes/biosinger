4439dd0a3602f385df768656d64226424effeecc
liting <luz.liting@gmail.com>
2023-02-01 16:53:17 +0800

    feat: toggle bg when router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.scss'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 import BiosingerHeader from './components/Header/Header'
 import Home from './pages/Home'
 import Search from './pages/Search/Search'
@@ -7,8 +8,18 @@ import SearchSp2000 from './pages/Search/SearchSp2000'
 import Info from './pages/Info'
 
 function App() {
+  const pathname = useLocation().pathname
+
+  const [bgColor, setBgColor] = useState('')
+  useEffect(() => {
+    if (pathname.startsWith('/search'))
+      setBgColor('from-[#f5f7fa] to-[#c3cfe2]')
+    else
+      setBgColor('from-[#a6c0fe] to-[#f68084]')
+  }, [pathname])
+
   return (
-    <div className="biosinger w-full min-w-256 h-screen">
+    <div className={ `biosinger w-full min-w-256 h-screen transition-colors bg-gradient-to-tr ${bgColor}` }>
       <BiosingerHeader />
       <main className="h-full">
         <Routes>
