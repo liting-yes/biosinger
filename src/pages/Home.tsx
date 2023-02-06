de12a69cc89a6501eb44aa9913aca8b54436f632
liting <luz.liting@gmail.com>
2023-02-06 21:57:13 +0800

    style(font): add LXGWWenKai and SmileySans Oblique fonts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
+import './Home.scss'
 import Texty from 'rc-texty'
 import 'rc-texty/assets/index.css'
 
 function Home() {
   return (
     <div className="biosinger-home h-full flex items-center justify-center">
-      <Texty className="inline-block text-8xl text-white">Bio Singer</Texty>
+      <Texty className="inline-block text-9xl text-white">Bio Singer</Texty>
     </div>
   )
 }
