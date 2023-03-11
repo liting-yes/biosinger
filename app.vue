6e1dd0aeb686db49f6faa9f5a359e2138bbb3fc9
liting <luz.liting@gmail.com>
2023-03-11 17:17:05 +0800

    refactor: layout

--- app.vue
+++ app.vue
@@ -1,13 +1,6 @@
 <template>
-  <el-container class="biosinger bg-[#FFFBFE] h-screen">
-    <el-header class="bg-[rgb(243,237,247)]">
-      <BioHeader />
-    </el-header>
-    <el-main>
-      <NuxtPage />
-    </el-main>
-    <el-footer class="drop-shadow bg-[rgb(247,242,249)]">
-      <BioFooter />
-    </el-footer>
-  </el-container>
+  <NuxtLayout>
+    <NuxtLoadingIndicator />
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-container class="biosinger bg-[#FFFBFE] h-screen">
+    <el-header class="bg-[rgb(243,237,247)]">
+      <BioHeader />
+    </el-header>
+    <el-main>
+      <slot />
+    </el-main>
+    <el-footer class="drop-shadow bg-[rgb(247,242,249)]">
+      <BioFooter />
+    </el-footer>
+  </el-container>
+</template>
